0566f778747d266811bc15e1f0d8817f9739f92e
Bruno Longo <longo.bruno@gmail.com>
2026-01-29 14:45:12 -0300

    ammend seed point & models

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from app.models import User, Snippet, SnippetCollaboratorLink, SnippetHistory
 from app.services import SnippetSerializer
 
 
-VERSION = "0.12"
+VERSION = "0.125"
 
 
 DATABASE_FILE = "sqlite:///app_data.db"
